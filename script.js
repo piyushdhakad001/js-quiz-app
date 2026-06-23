57232af485973ef3a5a8ec55329c4449bbde3632
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-24 03:25:52 +0530

    Create question data structure

---
+++ script.js
@@ -0,0 +1,27 @@
+const objQue = [
+  {
+    Question: "1. PM of India?",
+    option: ["Trump", "Modi", "Putin", "Xee"],
+    Answer: "Modi",
+  },
+  {
+    Question: "2. Capital of MP?",
+    option: ["Jabalpur", "Indore", "Bhopal", "Gwalior"],
+    Answer: "Bhopal",
+  },
+  {
+    Question: "3. National Sport of India?",
+    option: ["Cricket", "Hockey", "Kabbadi", "Football"],
+    Answer: "Hockey",
+  },
+  {
+    Question: "4. Which sport has highest popularity?",
+    option: ["Cricket", "Hockey", "Kabbadi", "Football"],
+    Answer: "Football",
+  },
+  {
+    Question: "5. Which sport has highest popularity in India?",
+    option: ["Cricket", "Hockey", "Kabbadi", "Football"],
+    Answer: "Cricket",
+  },
+];
