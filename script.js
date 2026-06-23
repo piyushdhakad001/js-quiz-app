2d61061b4f4773f0141f2f4b58763509a51d4f30
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-24 03:30:59 +0530

    Implement dynamic question rendering

--- script.js
+++ script.js
@@ -25,3 +25,42 @@ const objQue = [
     Answer: "Cricket",
   },
 ];
+
+const container = document.querySelector(".container");
+const startQuiz = document.querySelector(".start-quiz");
+
+let count = 0;
+let userAnswer = {};
+
+startQuiz.addEventListener("click", () => {
+  startQuiz.innerHTML = "";
+  renderQuestion();
+});
+
+function renderQuestion() {
+  localStorage.setItem("count", count);
+  localStorage.setItem("userAnswer", JSON.stringify(userAnswer));
+
+  container.innerHTML = "";
+  const questionElement = document.createElement("div");
+
+  const question = document.createElement("p");
+  question.classList.toggle("question");
+  question.textContent = `${objQue[count].Question}`;
+
+  const option1 = document.createElement("p");
+  option1.textContent = `${objQue[count].option[0]}`;
+
+  const option2 = document.createElement("p");
+  option2.textContent = `${objQue[count].option[1]}`;
+
+  const option3 = document.createElement("p");
+  option3.textContent = `${objQue[count].option[2]}`;
+
+  const option4 = document.createElement("p");
+  option4.textContent = `${objQue[count].option[3]}`;
+
+  questionElement.appendChild(question);
+  
+  container.appendChild(questionElement);
+}
